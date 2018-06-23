dacb58a44f5a11bd1ba39b55430b54f338093964
Unknown <karoljesionkadev@gmail.com>
2018-06-23 18:20:52 +0200

    Correct client methods

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,31 +11,31 @@ class PagesController extends Controller
         $client = new Client();
         $res = $client->request('GET', 'http://127.0.0.1:9999/api/products');
         $stringBody = (string)$res->getBody();
-        $data = json_decode($stringBody, true)['data'];
+        $data = json_decode($stringBody, true);
         return view('index', ['data'=>$data]);
     }
 
     public function getAvailable() {
         $client = new Client();
-        $res = $client->request('GET', 'http://127.0.0.1:9999/api/products/amount/0/1');
+        $res = $client->request('POST', 'http://127.0.0.1:9999/api/products/amount/0/1');
         $stringBody = (string)$res->getBody();
-        $data = json_decode($stringBody, true)['data'];
+        $data = json_decode($stringBody, true);
         return view('index', ['data'=>$data]);
     }
 
     public function getNotAvailable() {
         $client = new Client();
-        $res = $client->request('GET', 'http://127.0.0.1:9999/api/products/amount/0');
+        $res = $client->request('POST', 'http://127.0.0.1:9999/api/products/amount/0');
         $stringBody = (string)$res->getBody();
-        $data = json_decode($stringBody, true)['data'];
+        $data = json_decode($stringBody, true);
         return view('index', ['data'=>$data]);
     }
 
     public function getMoreThan() {
         $client = new Client();
-        $res = $client->request('GET', 'http://127.0.0.1:9999/api/products/amount/5/1');
+        $res = $client->request('POST', 'http://127.0.0.1:9999/api/products/amount/5/1');
         $stringBody = (string)$res->getBody();
-        $data = json_decode($stringBody, true)['data'];
+        $data = json_decode($stringBody, true);
         return view('index', ['data'=>$data]);
     }
 }
